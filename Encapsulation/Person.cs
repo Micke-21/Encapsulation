e3035c2017ac993b0d33ec686450b88b234f97e0
Mikael Larsson <mikael.larsson@wsp.com>
2021-12-02 16:06:50 +0100

    Add project files.

---
+++ Encapsulation/Person.cs
@@ -0,0 +1,37 @@
+﻿namespace Encapsulation
+{
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+
+        public Person(string firstName, string lastName, int age)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+        }
+
+        public string FirstName
+        {
+            get => firstName; 
+            //internal set => firstName = value;
+        }
+        public string LastName
+        {
+            get => lastName;
+            //internal set => lastName = value;
+        }
+        public int Age
+        {
+            get => age; 
+            //internal set => age = value;
+        }
+
+        public override string ToString()
+        {
+            return FirstName + " " + LastName + " is " + Age + " years old.";
+        }
+    }
+}
